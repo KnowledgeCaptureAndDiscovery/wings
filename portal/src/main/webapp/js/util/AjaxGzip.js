8a4132795e473ae9c5e32b7f71c982be48897c11
Varun Ratnakar <varunratnakar@gmail.com>
2017-03-03 18:56:51 +0000

    Work with slightly older browsers

--- portal/src/main/webapp/js/util/AjaxGzip.js
+++ portal/src/main/webapp/js/util/AjaxGzip.js
@@ -1,6 +1,29 @@
 var GZIPWorker = undefined;
 
 Ext.Ajax.requestGZ = function(config) {
+	
+	this.sendZipdata = function(body, conf) {
+		conf.binaryData = body;
+		conf.binary = true;
+		
+		var successfn = conf.success;
+		if(successfn) {
+			conf.success = function(response) {
+				response.responseText = new TextDecoder('UTF-8').decode(response.responseBytes);
+				successfn.call(this, response);
+			}
+		}
+		var failurefn = conf.failure;
+		if(failurefn) {
+			conf.failure = function(response) {
+				if(response.responseBytes)
+					response.responseText = new TextDecoder('UTF-8').decode(response.responseBytes);
+				failurefn.call(this, response);
+			}
+		}
+		Ext.Ajax.request(conf);
+	};
+	
 	config.method = 'post';
 	if(!config.headers)
 		config.headers = {};
@@ -26,37 +49,29 @@ Ext.Ajax.requestGZ = function(config) {
 	}
 
 	if(body) {
+		var me = this;
 		if(typeof body == 'object')
 			body = Ext.encode(body);
 		
-	    if(GZIPWorker == undefined) {
-	    	GZIPWorker = new Worker(CONTEXT_ROOT + "/js/workers/gzip.js");
-	    	//console.log("creating new gzip worker");
-	    }
-	    GZIPWorker.postMessage(body);
-	    GZIPWorker.onmessage = function(e) {
-			config.binaryData = e.data;
-			config.binary = true;
-			
-			var successfn = config.success;
-			if(successfn) {
-				config.success = function(response) {
-					response.responseText = new TextDecoder('UTF-8').decode(response.responseBytes);
-					successfn.call(this, response);
+		if(window.Worker) {
+		    if(GZIPWorker == undefined) {
+		    	GZIPWorker = new Worker(CONTEXT_ROOT + "/js/workers/gzip.js");
+		    }
+		    GZIPWorker.postMessage(body);
+		    GZIPWorker.onmessage = function(e) {
+		    	me.sendZipdata(e.data, config);
+		    }
+		} else {
+			Ext.Loader.loadScript({
+				url: CONTEXT_ROOT + "/js/workers/jsonc.min.js",
+				onLoad : function() {
+					var zipbody = gzip.zip(body);
+					me.sendZipdata(zipbody, config);
 				}
-			}
-			var failurefn = config.failure;
-			if(failurefn) {
-				config.failure = function(response) {
-					if(response.responseBytes)
-						response.responseText = new TextDecoder('UTF-8').decode(response.responseBytes);
-					failurefn.call(this, response);
-				}
-			}
-			Ext.Ajax.request(config);
-	    }
+			});
+		}
 	}
 	else {
 		Ext.Ajax.request(config);
 	}
-};
+}
--- portal/src/main/webapp/js/workers/register-workers.js
+++
@@ -1,3 +0,0 @@
-function registerWorker(path) {
-
-}
