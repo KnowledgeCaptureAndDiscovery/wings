a2013a3834b5bbe88ba81732bd9e08c24e04e13d
Varun Ratnakar <varunratnakar@gmail.com>
2019-02-19 11:53:31 +0530

    Fix transaction lock on datatype error

--- portal/src/main/java/edu/isi/wings/portal/controllers/DataController.java
+++ portal/src/main/java/edu/isi/wings/portal/controllers/DataController.java
@@ -465,6 +465,8 @@ public class DataController {
 			}
 		}
 		catch(Exception e) {
+      this.dc.stop_batch_operation();
+      this.dc.end();		  
 			e.printStackTrace();
 			errors.add(e.getMessage());
 		}
