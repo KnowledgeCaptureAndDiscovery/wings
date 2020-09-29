9e472a1108cd101c2cc4527fa4839bbe6183d378
Maximiliano Osorio <mosorio@isi.edu>
2020-09-29 10:42:11 -0700

    add docker login

--- docker-push.sh
+++ docker-push.sh
@@ -1,4 +1,6 @@
+
 COMMIT=${TRAVIS_COMMIT::6}
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t kcapd/wings:latest -f wings-docker/docker/default/Dockerfile .
 docker tag kcapd/wings kcapd/wings:$COMMIT
 docker push kcapd/wings:$COMMIT
