6ced6496ca65c5f021f91ff07637fed21651c0a8
Maximilinano Osorio <mosorio@isi.edu>
2022-02-24 17:00:46 -0300

    Release 4.2.6

--- .update-version.sh
+++ .update-version.sh
@@ -1,5 +1,6 @@
-set -xe
-pushd core
-mvn --settings pom.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${version}
-popd
+#!/bin/bash
+version=${1}
 sed -i "s/You are running WINGS version: .*/You are running WINGS version: ${version} /g" portal/src/main/webapp/html/home.html
+cd core
+mvn --settings pom.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${version}
+exit 0
