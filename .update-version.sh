110a949966c1175294370976079b6d31d63de56b
Maximilinano Osorio <mosorio@isi.edu>
2022-03-22 12:02:22 -0300

    fix: release-it support macos

--- .update-version.sh
+++ .update-version.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+set -e
 version=${1}
-sed -i "s/You are running WINGS version: .*/You are running WINGS version: ${version} /g" portal/src/main/webapp/html/home.html
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	sed -i "s/You are running WINGS version: .*/You are running WINGS version: ${version} /g" portal/src/main/webapp/html/home.html
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+	if ! command -v gsed &> /dev/null
+	then
+	    echo "Please install gsed on macos"
+	    exit 1
+	fi
+	gsed -i "s/You are running WINGS version: .*/You are running WINGS version: ${version} /g" portal/src/main/webapp/html/home.html
+else
+	echo "OS not supported"
+        exit 1
+fi
 cd core
 mvn --settings pom.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=${version}
 exit 0
