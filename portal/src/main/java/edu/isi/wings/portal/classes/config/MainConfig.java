49aad394dd14656ee89447ccf4237778ba7f7990
Maximiliano Osorio <maxiosorio@gmail.com>
2023-07-12 09:14:04 -0400

    fix: main config

---
+++ portal/src/main/java/edu/isi/wings/portal/classes/config/MainConfig.java
@@ -0,0 +1,44 @@
+package edu.isi.wings.portal.classes.config;
+
+import org.apache.commons.configuration.plist.PropertyListConfiguration;
+
+public class MainConfig {
+
+  // Comma separated list of spellbook client hosts
+  private String clients;
+  private String dotFile;
+  private String serverUrl;
+
+  public MainConfig() {
+  }
+
+  public MainConfig(PropertyListConfiguration serverConfig) {
+    this.clients = serverConfig.getString(Config.MAIN_CLIENTS_KEY);
+    this.dotFile = serverConfig.getString(Config.MAIN_GRAPHVIZ_KEY);
+    this.serverUrl = serverConfig.getString(Config.MAIN_SERVER_URL_KEY);
+  }
+
+  public String getClients() {
+    return clients;
+  }
+
+  public void setClients(String clients) {
+    this.clients = clients;
+  }
+
+  public String getDotFile() {
+    return dotFile;
+  }
+
+  public void setDotFile(String dotFile) {
+    this.dotFile = dotFile;
+  }
+
+  public String getServerUrl() {
+    return serverUrl;
+  }
+
+  public void setServerUrl(String serverUrl) {
+    this.serverUrl = serverUrl;
+  }
+}
